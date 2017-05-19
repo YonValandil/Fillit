665c5b4e13d7cc186a32f5c8b6ac7e5a2b2fb206
Emmanuel FERRANDINI <eferrand@e1r12p15.42.fr>
2017-05-19 10:23:24 +0200

    maj fichier tetris mode binaire

--- srcs/tetris.c
+++ srcs/tetris.c
@@ -1,31 +1,32 @@
 #include "fillit.h"
 
-int		check_tetri(t_tetri *t, int k, int j, int four)
+int		check_tetri(t_tetri *t)
 {
 	int	i;
+	int	j;
+	int	four;
 	int	connect;
 
 	i = -1;
-	while (++i < 16)
-	{
-		if (t[j]->tetriminos[i] && ++four)
-		{
-			connect = 0;
-			if (((i + 1) % 4) && t->tetriminos[j][i + 1])
-				++connect;
-			if ((i % 4) && t->tetriminos[j][i - 1])
-				++connect;
-			if ((i > 3) && t->tetriminos[j][i - 4])
-				++connect;
-			if ((i < 12) && t->tetriminos[j][i + 4])
-				++connect;
-			if (connect == 2)
-				k = 1;
-			else if (!connect)
-				return (0);
-		}
-	}
-	return ((!k || four != 4) ? 0 : 1);
+	j = -1;
+	four = 0;
+	while (++j < 26)
+		while (++i < 16)
+			if ((t[j]->data & (1 << i)) && ++four)
+			{
+				connect = 0;
+				if (i < 16 && (t[j]->data & (1 << (i + 1))) && t[j]->data)
+					++connect;
+				if (i < 12 && (t[j]->data & (1 << (i + 4))) && t[j]->data)
+					++connect;
+				if (0 < i && (t[j]->data & (1 << (i - 1))) && t[j]->data)
+					++connect;
+				if (3 < i && (t[j]->data & (1 << (i - 4))) && t[j]->data)
+					++connect;
+				if ((!connect) || (four != 4 && i == 15))
+					return (0);
+			}
+	return ((four != 4) ? 0 : 1);
 }
 
 int		check_tetris(t_tetri *t)
