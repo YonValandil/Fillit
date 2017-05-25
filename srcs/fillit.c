6bb0d9d192f92c821024916ff69ca703e2b82410
Jimmy JOURNE <jjourne@student.42.fr>
2017-05-25 05:50:56 +0200

    update not really effective

--- srcs/fillit.c
+++ srcs/fillit.c
@@ -8,7 +8,8 @@ void	begin(t_tetri *t)
 	int			p;
 //	calcul carré minimum en fonction du nombre de pieces
 	map = NULL;
-	square = 4; //on fait * 2 direct
+	square = 2; //l'air minimum pour une piece c'est 3
+	//je prend pour une piece au commencement parce qu'en fait on a pas le nbr de pieces totals
 	/* Note de Manu :
 ** Square doit posseder la taille minimum du coté de la carte en fonction du nombre de piece
 ** 1 piece = 4 cubes
@@ -24,6 +25,7 @@ void	begin(t_tetri *t)
 
 	p = 0;
 //	si backtracking echec alors piece precedente a bouger => augmenter tailler carré minimum
+
 	//bt = backtracking(t, square, map, p);
 	/*if(bt > 0)
 		backtracking(t, square, map, p + 1);
@@ -31,7 +33,7 @@ void	begin(t_tetri *t)
 	{
 		backtracking(t, square * 2, map, p + 1);
 	}*/
-	while (0 < backtracking(t, square * 2, map, p));
+	while (0 < backtracking(t, ft_pow(++square), map, p)); //ma ft_pow fait que le carrer
 }
 
 int 	backtracking(t_tetri *t, int square, short *map, int p)
@@ -45,20 +47,22 @@ int 	backtracking(t_tetri *t, int square, short *map, int p)
 	if (!map)
 		map[13] = (short[13]){0};
 	//comparaison binaire pour placer la piece
-	//pas moyen de faire direct sans les boucles?
+	//est ce que je suis oblige de parcourir toute la map (un a un) avec les boucles ?
+	//Ou est qu'il y a un moyen de comparer un block entier et non pas caractere par caractere
 	//Manu : je comprend pas la question, pas assez precise et a quoi sert a la ligne d en dessous ?
-	while (++c.y <= t->y) //pour chaque lignes du tetri
+	//OK ----- boucle pas du tout correct (ne parcourt pas la map comme je le pensais)
+	while (++c.y <= t->y) //parcours chaques lignes
 	{
-		while (++c.x <= t->x)
-		{
-			//if((t[p]->data &= 0xF000 >> 4 * c.y))
-			if((map[c.y][c.x] &= (t[p]->data & 0b1 << 16 - c.y))) //si 1 alors il y a un obstacle
-				i = 1;
-		}
+		while (++c.x <= t->x) //parcours chaques colonnes
+			if((map[c.y][c.x] && (t[p]->data & 0b1 << 16 - c.x))) //si 1 alors il y a un obstacle
+				i = 1; //il y a deja quelque chse sur la map a cette case
+		c.x = 0; //on remet au debut de la ligne
 	}
-	//if (!i) //si toute la place est libre pour la piece
+	//si toute la place est libre pour le tetri et qu'il depasse pas on peut poser la piece
+	if (!i && (map[c.y][c.x] + t->x <= square))
 	//Manu : penser a checker si la piece + sa position est superieur a square, dans ce cas c est pas bon, pour savoir
 	//en x : c.x + t->x <= square
+
 
 		//map[] = t[p]->data;
 	//backtracking(t, square, map, p + 1);
